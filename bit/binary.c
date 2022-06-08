5b5215990a0d50033bea24df849b3837b1b1d297
Shahaf Goldstein <shahafg14@gmail.com>
2022-06-08 20:40:05 +0300

    Final

--- bit/binary.c
+++ bit/binary.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
+
+
 int pack4Chars2Int(char c1, char c2, char c3, char c4);
 int checkEvenParity(short input);
 void main()
 {
-	short input = 7;
+	short input = -55;
 	char c1 = 'a';
 	char c2 = 'b';
 	char c3 = 'c';
 	char c4 = 'd';
-	printf("%d", pack4Chars2Int(c1, c2, c3, c4));
-	printf("\n%d", checkEvenParity(input));
+	
+	printf("%d", checkEvenParity(input));
 }
 
 int pack4Chars2Int(char c1, char c2, char c3, char c4)
@@ -26,8 +28,11 @@ int pack4Chars2Int(char c1, char c2, char c3, char c4)
 int checkEvenParity(short input)
 {
 	char mask = 0b1;
-	char mask1 = 0b1;
 	int count = 0;
+	if (input<0)
+	{
+		input = ~input;
+	}
 	while (input != 0)
 	{
 		if (mask & input)
